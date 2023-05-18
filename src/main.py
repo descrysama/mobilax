f2fe525a0802912c2ebfb67861101ae7fa52e21d
descrysama <erwing2002@gmail.com>
2023-05-18 17:45:08 +0200

    update pat

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import json;
 import requests
 
 
-with open('var/www/scripts/mobilax/src/urls.json', 'r') as file:
+with open('/var/www/scripts/mobilax/src/urls.json', 'r') as file:
     json_data = json.load(file)
 
 urls = json_data['urls']
 
-remove_at_start('var/www/scripts/mobilax/src/output.xlsx')
+remove_at_start('/var/www/scripts/mobilax/src/output.xlsx')
 
 for i, url in enumerate(urls) :
     items_list = []
@@ -22,6 +22,6 @@ for i, url in enumerate(urls) :
             item[0] = str(product['ean13'])
             item[1] = str(product['price']['price']).replace('.', ',')
             items_list.append(item)
-        add_data('var/www/scripts/mobilax/src/output.xlsx', items_list)
+        add_data('/var/www/scripts/mobilax/src/output.xlsx', items_list)
     else:
         print("url not found mind replacing it here is the index's list: ", i)
